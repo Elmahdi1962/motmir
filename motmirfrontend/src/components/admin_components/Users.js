d790a59377bc667ff5bb760ae3e8fa9950498131
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2022-03-01 16:08:42 +0100

    added a drop down menu for managing users added delete button on it but not working yet

--- motmirfrontend/src/components/admin_components/Users.js
+++ motmirfrontend/src/components/admin_components/Users.js
@@ -3,10 +3,53 @@ import { baseUrl } from '../../index.js';
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 import { getToken } from '../common';
+import { useNavigate } from 'react-router-dom';
 
 function Users() {
   const [users, setUsers] = useState([]);
-  const [error, setError] = useState('');
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const handleUserAdding = (e) => {
+    e.preventDefault();
+    setLoading(true);
+    if(e.target.elements.password.value !== e.target.elements.confirm_password.value) {
+      setLoading(false);
+      setError('Passwords do not match!');
+      return;
+    }
+
+    const data = {
+      "username": e.target.elements.username.value,
+      "email": e.target.elements.email.value,
+      "password": e.target.elements.password.value,
+      "is_admin": e.target.elements.is_admin.checked
+    };
+
+    axios.post(baseUrl + '/user/register', data, {
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      }
+    }).then(response => {
+      setLoading(false);
+      window.location.reload();
+    }).catch(error => {
+      setLoading(false);
+      if(error.response.data) {
+        setError(error.response.data.message);
+      } else {
+        setError('Failed to Register. Please try again later.');
+      }
+    });
+  }
+
+
+  const handleUserDelete = (e) => {
+
+  }
+
 
   useEffect(() => {
 
@@ -23,7 +66,13 @@ function Users() {
     })
     .catch(error => {
       console.log(error);
-      setError(error.response.data.message);
+      if(error.response.status === 401) {
+        navigate('/login')
+      } else if(error.response.data) {
+        setError(error.response.data.message);
+      } else {
+        setError('Failed to Register. Please try again later.');
+      }
     });
 
   }, []);
@@ -31,20 +80,23 @@ function Users() {
   return (
       <div className="userscontainer">
 
-        <form className="userform">
+        <form className="userform" onSubmit={handleUserAdding}>
           <label htmlFor="username">UserName</label>
-          <input type="text" name="username" required="required"/>
+          <input placeholder="username" type="text" name="username" required="required"/>
 
           <label htmlFor="email">Email</label>
-          <input type="email" name="email" required="required"/>
+          <input placeholder="email" type="email" name="email" required="required"/>
 
           <label htmlFor="password">Password</label>
-          <input type="text" name="password" required="required"/>
+          <input placeholder="password" type="password" name="password" required="required" maxLength="30" minLength="8"/>
+
+          <label htmlFor="confirm_password">Confirm Password</label>
+          <input placeholder="confirm password" type="password" name="confirm_password" maxLength="30" minLength="8" required="required"/>
 
           <label htmlFor="is_admin">Is Admin</label>
-          <input type="checkbox" name="is_admin" required="required"/>
+          <input type="checkbox" name="is_admin"/>
 
-          <input type="submit" value="add user" required="required"/>
+          <input type="submit" value="add user"/>
         </form>
 
         {error !== '' ? <p>{error}</p> : <></>}
@@ -52,6 +104,13 @@ function Users() {
         <div className="useritemscontainer">
           {users.map(user => 
           <div className="useritm" key={user.id}>
+
+            <div className="userdropdown">
+              <button className="userdropbtn"><i className="fa-solid fa-caret-down"></i></button>
+              <div className="userdropdown-content">
+                <button username={user.username} email={user.email} onClick={handleUserDelete}>Delete</button>
+              </div>
+            </div>
 
             <div className="uinfoblocks">
               <p><b>UserName : </b> {user.username}</p>

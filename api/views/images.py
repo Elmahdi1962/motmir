24eeb018f2c051827833292718bbb23398f8685d
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2022-03-12 14:55:13 +0100

    fix image sending

--- api/views/images.py
+++ api/views/images.py
@@ -17,8 +17,8 @@ def get_image(imagename=''):
         from PIL import Image
         from io import BytesIO
         r = get('https://ik.imagekit.io/motmir/images/' + imagename)
-        i = BytesIO(r.content)
-        return make_response(i)
+        i = r.raw
+        return make_response(i, headers={'Content-Type': r.header['Content-Type']}), 200
         # return send_from_directory(current_app.config[IMAGE_STORAGE_PATH'],
                             # imagename,
                             # as_attachment=False)

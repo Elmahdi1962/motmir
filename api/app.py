1a28b91ebaa3c4c5f13908d67eea4b7e43e98b8b
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2022-03-12 13:40:54 +0100

    fix cercular import

--- api/app.py
+++ api/app.py
@@ -16,6 +16,12 @@ from models.user import User
 from imagekitio import ImageKit
 
 
+imagekit = ImageKit(
+    private_key='private_jNliRzpQ4Q8UWtFlGWFZWq5QcCk=',
+    public_key='public_nzu98T+T43IzjgA81gfxQh5gRCg=',
+    url_endpoint = 'https://ik.imagekit.io/motmir'
+)
+
 app = Flask(__name__)
 bcrypt = Bcrypt(app)
 
@@ -34,14 +40,6 @@ app.config['ALLOWED_IMAGE_EXT'] = ['png', 'jpg', 'jpeg']
 app.register_blueprint(app_views)
 app.register_blueprint(auth_views)
 cors = CORS(app, resources={r"/*": {"origins": "*"}})
-
-
-imagekit = ImageKit(
-    private_key='private_jNliRzpQ4Q8UWtFlGWFZWq5QcCk=',
-    public_key='public_nzu98T+T43IzjgA81gfxQh5gRCg=',
-    url_endpoint = 'https://ik.imagekit.io/motmir'
-)
-
 
 @app.route('/')
 def home():

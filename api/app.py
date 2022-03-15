316322fbb3e4f9ebc367a18670c7b0b2b9a0a92f
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2022-03-15 16:25:12 +0100

    added more file extenstions to be allowed

--- api/app.py
+++ api/app.py
@@ -36,7 +36,7 @@ else:
 app.config['SECRET_KEY'] = 'e8d623e7c9988114bfa39f27fc4cf9c8'
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 app.config['IMAGE_STORAGE_URL'] = 'https://ik.imagekit.io/motmir/images/' # os.path.join(pathlib.Path(__file__).parent.resolve(), 'static/images')
-app.config['ALLOWED_IMAGE_EXT'] = ['png', 'jpg', 'jpeg']
+app.config['ALLOWED_IMAGE_EXT'] = ['png', 'jpg', 'jpeg', 'webp', 'gif']
 app.register_blueprint(app_views)
 app.register_blueprint(auth_views)
 cors = CORS(app, resources={r"/*": {"origins": "*"}})

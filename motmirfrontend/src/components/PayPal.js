5a1e48b8d9f20fe7bd14dbb6eae0ab34d255c277
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2022-03-15 10:47:42 +0100

    removed unsued code

--- motmirfrontend/src/components/PayPal.js
+++ motmirfrontend/src/components/PayPal.js
@@ -122,8 +122,6 @@ function PayPal({setCheckout, cart, setCart, shippingCost, totalPrice, totalQuan
             intent: "capture"
           }}
 
-          amount="25.00"
-
           onApprove={(data, actions) => {
             return actions.order.capture().then((details) => {
               alert("Transaction completed by " + details.payer.name.given_name);

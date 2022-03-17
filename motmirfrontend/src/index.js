e18d8db8c3e3629f6e4adc139019624aa30e676d
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2022-03-17 14:49:52 +0100

    undo last commit on this file

--- motmirfrontend/src/index.js
+++ motmirfrontend/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-export const baseUrl = 'http://localhost:5000'; //'https://motmir-api.herokuapp.com'
+export const baseUrl = 'https://motmir-api.herokuapp.com'; //'http://localhost:5000'
 export const imagesUrl = 'https://ik.imagekit.io/motmir/';
 
 ReactDOM.render(

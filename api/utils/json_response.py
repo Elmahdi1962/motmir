1fae9c551a8c2f9aa57bea4d4167f0573eb6bfeb
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2022-03-19 13:45:34 +0100

    update argument name

--- api/utils/json_response.py
+++ api/utils/json_response.py
@@ -1,19 +1,19 @@
-def create_response(type:str, data:dict=None, error:str=None):
+def create_response(status:str, data:dict=None, error:str=None):
     '''
     creates a dict response using the jsend Format
     
     Args:
-        type    (string) : is the response a "success" or "fail" or "error".
+        status    (string) : is the response a "success" or "fail" or "error".
         data    (dict)   : data to send (optional).
         error   (string) : error message (optional).
     '''
-    if type == 'success' or type == 'fail':
+    if status == 'success' or status == 'fail':
         return {
-            'status' : type,
+            'status' : status,
             'data' : data
         }
 
-    elif type == 'error':
+    elif status == 'error':
         return {
             'status' : "error",
             'message' : error
@@ -21,7 +21,7 @@ def create_response(type:str, data:dict=None, error:str=None):
 
     else:
         return {
-            'status' : type,
+            'status' : status,
             'data' : data,
             'message' : error
         }

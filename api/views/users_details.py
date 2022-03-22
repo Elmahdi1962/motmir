1fb5f43e7c276c961a13c8cbf7c86337cb7b882d
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2022-03-22 19:39:50 +0100

    add comments

--- api/views/users_details.py
+++ api/views/users_details.py
@@ -13,13 +13,14 @@ def get_users_details(current_user):
     """
     Retrieves the list of all usersdetails objects
     """
+    # if user is admin send everyones details
     if current_user.is_admin:
         all_orders = storage.all('UserDetails').values()
         list_orders = []
         for order in all_orders:
             list_orders.append(order.to_dict())
         return jsonify(list_orders), 200
-    
+    # if not admin
     else:
         return jsonify({'status': 401, 'message': 'Not allowed'}), 401
         
@@ -31,6 +32,7 @@ def add_update_users_details(current_user):
     this route for admin to update some else details
     """
     data = request.get_json()
+    # if no data received
     if not data:
         return jsonify({'status': 400, 'message': 'No data received or not Json'}), 400
 
@@ -55,5 +57,6 @@ def add_update_users_details(current_user):
         
         return jsonify({'status': 200, 'message': 'User details updated successfully.', 'data': new_details.to_dict()}), 200
     except:
+        # if something went wrong
         print('something wrong happened while trying to update user details in users_details.py try block line 41')
         return jsonify({'status': 500, 'message': 'Something wrong happened while trying to update your details'}), 500

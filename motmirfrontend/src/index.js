32cc19b1f0a90685694f88e05d0fa3fa76a7470b
Elmahdi1962 <mamounelmahdi1962@gmail.com>
2022-12-18 11:38:14 +0100

    update api url

--- motmirfrontend/src/index.js
+++ motmirfrontend/src/index.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
 
-export const baseUrl = 'https://motmir-api.herokuapp.com'; // 'http://localhost:5000' Api's base url
-export const imagesUrl = 'https://ik.imagekit.io/motmir/';
+export const baseUrl = "https://motmir-api.onrender.com"; // 'http://localhost:5000' Api's base url
+export const imagesUrl = "https://ik.imagekit.io/motmir/";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
